ade4b5d58cde5c5b1965f828eca67d0305996db9
Ruckshan Ratnam <hendryratnam@gmail.com>
2020-05-16 12:59:18 -0400

    intial realse build

--- popup.js
+++
@@ -1,9 +0,0 @@
-function init_scroll_up() {
-    document.getElementById("scroll_up").addEventListener('click', function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {scroll_up: "true"});
-          });
-    })
-}
-
-init_scroll_up()
